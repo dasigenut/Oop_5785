f7ff6c79c6731aa39141260c61d12b752ec64302
Nadav Aharoni <nadav.aharoni@gmail.com>
2024-11-25 10:59:58 +0200

    Added ExpandArray
    It's a program that tries to expand an array by replacing it with a larger array.
    This program does not work and the exercise is to explain why

---
+++ ExpandArray/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExpandArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            ExpandArray(a);
+
+            Console.WriteLine();
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        static void ExpandArray(int[] array)
+        {
+            int[] oldArray = array;
+
+            array = new int[oldArray.Length * 2];
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                array[i] = oldArray[i];
+                array[i + oldArray.Length] = oldArray[i];
+            }
+        }
+    }
+}

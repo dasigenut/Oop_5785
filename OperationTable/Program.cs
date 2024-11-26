35f912fb98fbca1f787db4d5047f58b64236f6c9
Nadav Aharoni <nadav.aharoni@gmail.com>
2024-11-26 19:40:11 +0200

    OperationsTable - initial commit

---
+++ OperationTable/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    class OperationTable
+    {
+        public int[,] results = new int[12, 10];
+
+        public void Calculate()
+        {
+            for (int row = 0; row < results.GetLength(0); row++)
+            {
+                for (int col = 0; col < results.GetLength(1); col++)
+                {
+                    results[row, col] = row * col;
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            for (int row = 0; row < results.GetLength(0); row++)
+            {
+                for (int col = 0; col < results.GetLength(1); col++)
+                {
+                    res += results[row, col].ToString();
+                    res += ",";
+                }
+                res += "\n";
+            }
+            return res;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        OperationTable table = new OperationTable();
+        table.Calculate();
+        Console.WriteLine(table);
+
+        Console.WriteLine($"{table.results[1,1]}");
+    }
+}
